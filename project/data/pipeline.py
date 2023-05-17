9190afbf489c05963616bbdcf8638839d773d196
Syed Ikhtiar Ahmed <ikhtiar0240@gmail.com>
2023-05-17 15:28:50 +0200

    implement data transformation #11

--- project/data/pipeline.py
+++ project/data/pipeline.py
@@ -1,5 +1,23 @@
 import extract
+import transform
+
+def get_weather_data():
+    return extract.get_weather_data()
+
+def transform_weather_data():
+    return transform.transform_weather_data(get_weather_data())
+
+def get_traffic_data():
+    return extract.get_traffic_data()
+
+def transform_traffic_data():
+    return transform.transform_traffic_data(get_traffic_data())
+
+def merge_data():
+    return transform.merge_datasets(
+        transform_weather_data(),
+        transform_traffic_data()
+        )
 
 if __name__ == "__main__":
-    extract.save_weather_data()
-    extract.save_traffic_data()
+    merge_data()
