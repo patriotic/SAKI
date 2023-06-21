7659d833cec9fb1da11fa8e4fe4a7d833e1e16cb
Syed Ikhtiar Ahmed <ikhtiar0240@gmail.com>
2023-06-21 22:51:54 +0200

    test if db does not exist

--- project/tests/test_pipeline.py
+++ project/tests/test_pipeline.py
@@ -4,8 +4,11 @@ from data.transform import Transformer
 from data.load import Loader
 import os
 
-# def test_db_not_exists():
-#     assert os.path.exists("./project/data/data.sqlite") == False
+def test_db_not_exists():
+    path = "./project/data/data.sqlite"
+    if os.path.exists(path):
+        os.remove(path)
+    assert os.path.exists(path) == False
     
     
 def test_run_pipeline():
